6d9bc969d2247bc01166795f0bee56729bac13f0
agungdwiprasetyo <agungdwiprasetyo22@gmail.com>
2019-04-18 22:06:43 +0700

    add offset calculation in meta

--- shared/meta/meta.go
+++ shared/meta/meta.go
@@ -1,8 +1,13 @@
 package meta
 
 type Meta struct {
-	Page         int
-	Limit        int
-	TotalPages   int
-	TotalRecords int
+	Page         int `json:"page"`
+	Limit        int `json:"limit"`
+	Offset       int `json:"-"`
+	TotalPages   int `json:"totalPages"`
+	TotalRecords int `json:"totalRecords"`
+}
+
+func (m *Meta) CalculateOffset() {
+	m.Offset = (m.Page - 1) * m.Limit
 }
